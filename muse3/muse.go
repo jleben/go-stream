39ff866f4f5df97b16e3ea587956545b7687fca5
Jakob Leben <jakob.leben@gmail.com>
2014-03-05 02:38:40 -0800

    muse: add Event, Compose, Play

--- muse3/muse.go
+++ muse3/muse.go
@@ -1,9 +1,15 @@
 package muse
 
 import (
+  "time"
   "../stream"
   "container/list"
 )
+
+type Event struct {
+  Duration int
+  Text string
+}
 
 func ListInt( slice []int ) *list.List {
   list := list.New()
@@ -32,4 +38,46 @@ func Iterate(list *list.List) chan stream.Event {
   }
   
   return stream.Source(work)
+}
+
+func Compose( input ... chan stream.Event) chan stream.Event {
+  output := make(chan stream.Event)
+
+  work := func() {
+    dur_in := input[0]
+    text_in := input[1]
+    for {
+      dur := (<-dur_in).(int)
+      text := (<-text_in).(string)
+      e := Event {dur, text}
+      output <- e
+    }
+  }
+
+  go work()
+
+  return output
+}
+
+func Play( source chan stream.Event, tatum time.Duration, reference time.Time ) chan stream.Event {
+  output := make(chan stream.Event)
+
+  work := func () {
+    t := 0
+    for token, ok := <-source; ok; token, ok = <-source {
+      event := token.(Event)
+
+      output <- event;
+
+      t = t + event.Duration;
+
+      real_time := reference.Add(time.Duration(t) * tatum);
+      real_duration := real_time.Sub(time.Now());
+      real_time = <-time.After(real_duration);
+    }
+  }
+
+  go work()
+
+  return output
 }
