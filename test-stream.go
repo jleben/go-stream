4aa32c716ade7706f28b79d2ca92202387773772
Jakob Leben <jakob.leben@gmail.com>
2014-03-05 17:16:48 -0800

    check in some testing code

---
+++ test-stream.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+  "fmt"
+  "time"
+  "./stream"
+  "./muse3"
+)
+
+func main() {
+
+  play := func (dur []int, text []string) stream.Operator {
+    dur_stream := muse.Iterate( muse.ListInt(dur) )
+    text_stream := muse.Iterate( muse.ListString(text) )
+    //f := stream.Fork(x, 2)
+    //z := stream.Join( f[0], f[1] )
+
+    events := muse.Compose(dur_stream, text_stream)
+
+    return events
+  }
+
+  var voices [] stream.Operator;
+  for i := 0; i < 10; i++ {
+    voices = append(voices, play( []int{1}, []string{"a"} ) );
+  }
+
+  tatum := 100 * time.Millisecond
+  start_time := time.Now();
+
+  // Version 1:
+
+  //x := muse.Play(tatum, start_time, voices...)
+
+  // Version 2:
+
+  for i := 0; i < 10; i++ {
+    voices [i] = muse.PlayOne(voices[i], tatum, start_time)
+  }
+  x := stream.Join(voices...)
+
+  //
+
+  s := x.Play()
+
+  for {
+    e := (<-s).(muse.Event);
+    fmt.Printf("%v: %v\n", time.Now().Sub(start_time).Seconds() * 1000, e);
+  }
+
+}
